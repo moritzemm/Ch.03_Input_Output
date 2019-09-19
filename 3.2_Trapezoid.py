fd076240341f3640583d0c1ff8ecf4d69394fd22
moritzemm <moritzemm@urbandalkeschools.com>
2019-09-19 13:13:44 -0500

    commit 9/19

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -9,6 +9,12 @@ base 1: 2       base 2: 3    height: 4    area: 10
 base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
-(base1*base 2)/2 *h
+(base1+base 2)/2 *h
 '''
-print("welcome to your ")
+print("welcome to your Trapezoid area calculator")
+B1=int(input("What is the length of base 1?"))
+B2=int(input("What is the length of base 2?"))
+H=int(input("What is the height of your trapezoid?"))
+B=(B1+B2)/2
+area=B*H
+print("The area of your trapezoid is",area)
